270922ff6fcbbd8c64b0607c36c6142dc6449023
Michele Greco <mitchgrek@gmail.com>
2023-12-20 01:55:55 +0100

    Added cross origin resource policy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,32 @@
-import bodyParser from "body-parser";
-import cors from "cors";
-import express, { Application } from "express";
-import helmet from "helmet";
-import morgan from "morgan";
-import { argv } from "node:process";
-import * as path from "path";
-import responseTime from "response-time";
-import { ApiRouter } from "./routes/api.router";
-import { MockedApiRouter } from "./routes/mocked-api.router";
-import { LogManager } from "./utils/logManager.util";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express, { Application } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { argv } from 'node:process';
+import * as path from 'path';
+import responseTime from 'response-time';
+import { ApiRouter } from './routes/api.router';
+import { MockedApiRouter } from './routes/mocked-api.router';
+import { LogManager } from './utils/logManager.util';
 
 const app: Application = express();
 const PORT = 8080;
-const MOCKED = argv.includes("--mocked");
+const MOCKED = argv.includes('--mocked');
 const corsOptions = {
-  origin: ["http://localhost:4200"],
+  origin: ['http://localhost:4200'],
   optionsSuccessStatus: 200,
   credentials: true,
 };
 
 // Secure setting various HTTP headers
-app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
 // Middleware that records the response time for HTTP requests
 app.use(responseTime());
 
 // Logging http request in the console
-app.use(morgan("combined", { stream: LogManager.getLogStream() }));
+app.use(morgan('combined', { stream: LogManager.getLogStream() }));
 
 // Enabling receipt of parameters from forms
 app.use(express.urlencoded({ extended: false }));
@@ -36,18 +36,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 // Configuring routers
 if (!MOCKED) {
-  app.use("/api", ApiRouter);
+  app.use('/api', ApiRouter);
 } else {
-  app.use("/assets", express.static(path.join(__dirname, "public")));
-  app.use("/api", MockedApiRouter);
+  app.use('/assets', express.static(path.join(__dirname, 'public')));
+  app.use('/api', MockedApiRouter);
 }
 
-app.get("/", (req, res) => {
-  res.send("Infollow backend is running!");
+app.get('/', (req, res) => {
+  res.send('Infollow backend is running!');
 });
 
 // Starting the server
